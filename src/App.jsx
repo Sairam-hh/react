ec6d1b06199952471e678413e65da454f15e07f4
Sairam-hh <sairambanti41@gmail.com>
2026-03-24 16:10:15 +0530

    props

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <p><b>Name:</b> Sairam Banti</p>
+      <p><b>Email:</b> sairambanti@thestackly.com</p>
+      <p><b>Role/Qualification:</b> Frontend Developer/ computer science of  engineering gradute</p>
+      <p><b>Short Introduction:</b> I am Sairam B! I joined  a month ago as a Frontend Developer.I have basic knowledge  of technologies like HTML ,CSS JAVASCRIPT and now i am learning React to build modern single page applications.I enjoy learning new technologies and improving my skills to grow as a Developer. </p>
+      
+      </div>
+    </div>
+  )
+}
