4335e9eab2b1cc5be3af3e61ba4c44957ec3c7f6
Sairam-hh <sairambanti41@gmail.com>
2026-03-27 23:05:05 +0530

    Task 5

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,13 @@ import StudentCard from './studentCard'
 import RegistrationForm from './RegistrationForm'
 
 export default function App() {
-  const student=[
-    {id:1,name:"Sairam B",course:"javaSscript",isActive:true},
-    {id:2,name:"Rajkumar B",course:"ReactJs",isActive:false},
-    {id:3,name:"Ramya G",course:"NodeJs",isActive:true},
-    {id:4,name:"Pavan G",course:"ExpressJs",isActive:false},
-    {id:4,name:"Kevin J",course:"Python",isActive:false},
-    {id:4,name:"Naveen N",course:"MangoDB",isActive:true}
-
-  ]
+  
 
   return (
     <div style={{ padding: "30px", fontFamily: "Arial, sans-serif" }}>
       <h1 style={{textAlign:"center",fontSize:"40px"}}>Registration Form</h1>
       <RegistrationForm/>
-
-
-      {/* <h1 style={{ color: "#4a90e2", marginBottom: "30px" }}> 🎓 Student List</h1>
-      <div style={{display:"flex", flex:"wrap"}}>
-        {student.map((student)=>(
-          <StudentCard key={student.id} student={student}/>
-          
-
-        ))}
-      </div> */}
-
-
-
-
-
-
-
       
-
-
-
-
-
-
-
-
-
-
-      
-      
-      
-      
-
-
 
 
 
